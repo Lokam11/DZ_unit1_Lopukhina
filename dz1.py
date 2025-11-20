4334805ca5f650164ef21ea3cc5267709a78fc91
Lokam11 <lopukhina04@inbox.ru>
2025-11-21 00:25:54 +1000

    Учимся считать

--- dz1.py
+++ dz1.py
@@ -1,1 +1,22 @@
-print ("Hello World 2")
+s1 = 'Я люблю тебя'
+s2 = "I love you"
+s3 = """
+На дворе трава
+На траве ???
+"""
+
+#print (s3)
+#who = input ("Что было на траве?")
+#print ("На траве были", who)
+
+a = input("Enter a:")
+print(type(a))
+a = int(a)
+print(type(a))
+pi = 3.1415926
+
+print (f'a={a}, pi={pi}, a*pi={a*pi}')
+
+b = a/2
+bb = a//2
+print(f'{b=}, {bb=}')
