e7451be97b144ec4c427edbf81744258986e5a48
Lokam11 <lopukhina04@inbox.ru>
2025-11-22 15:37:22 +1000

    задание изи от 1 до 100

--- dz2.py
+++ dz2.py
@@ -1,6 +1,19 @@
-n = int(input("Введите целое число N:"))
-sum = 1
-for i in range(1,n):
-    sum = sum + i
-    print("Сумма чисел от 1 до", n, "равна", sum)
+#n = int(input("Введите целое число N:"))
+#sum = 1
+#for i in range(1,n):
+#    sum = sum + i
+#    print("Сумма чисел от 1 до", n, "равна", sum)
+
+import random
+
+N = random.randint(1, 100)
+i = input("Угадайте число:")
+i = int(i)
+if i > N:
+    print ("Ваше число больше")
+if i < N:
+    print ("Ваше число меньше")
+if i == N:
+    print ("Ура, угадал")
+
 
